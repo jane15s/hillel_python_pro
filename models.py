d84f503a099ab444efa00acf0afb592e05139a5d
jane <sysoievayevheniia@gmail.com>
2025-08-21 14:53:53 +0300

    added filters, chart, category/transactions operations

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from xmlrpc.client import DateTime
 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric, Date
 from database import Base
 
 class User(Base):
@@ -10,17 +10,20 @@ class User(Base):
     surname = Column(String(50))
     password = Column(String(50))
     email = Column(String(120), unique=True)
-    birth_date = Column(DateTime)
+    birth_date = Column(Date)
     gender = Column(String(10))
 
     def __repr__(self):
-        return f'<User {self.name!r}>'
+        return f"<User id={self.id} name={self.name!r} surname={self.surname!r} email={self.email!r}>"
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     owner = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return f"<Category id={self.id} name={self.name!r} owner={self.owner}>"
 
 class Transactions(Base):
     __tablename__ = 'transactions'
@@ -31,3 +34,8 @@ class Transactions(Base):
     datetime =Column(DateTime)
     owner = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
     type = Column(Integer)
+
+    def __repr__(self):
+        return (f"<Transactions id={self.id} description={self.description!r} "
+                f"category={self.category} amount={self.amount} "
+                f"datetime={self.datetime} owner={self.owner} type={self.type}>")
